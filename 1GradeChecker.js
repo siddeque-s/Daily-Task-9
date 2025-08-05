6ad7f18ae5ee19751a9a70a8aec8df9005ea93de
Siddeque Sajeev <siddeques45@gmail.com>
2025-08-05 18:14:22 +0530

    2 question completed

--- 1GradeChecker.js
+++ 1GradeChecker.js
@@ -1,17 +1,13 @@
-const mark=101;
+const mark = 101;
 
-if(mark>=90 && mark<=100){
-    console.log("Grade A")
-}else if(mark>=75 && mark<=89){
-    console.log("Grade B")
+if (mark >= 90 && mark <= 100) {
+  console.log("Grade A");
+} else if (mark >= 75 && mark <= 89) {
+  console.log("Grade B");
+} else if (mark >= 50 && mark <= 74) {
+  console.log("Grade C");
+} else if (mark >= 0 && mark <= 49) {
+  console.log("Fail");
+} else {
+  console.log("Invalid");
 }
-else if(mark>=50 && mark<=74){
-    console.log("Grade C")
-}
-else if(mark>=0 && mark<=49){
-    console.log("Fail")
-}
-else{
-    console.log("Invalid")
-}
-
